98e9bd4d5dd5ffe7005b3a28dbc8a5594cd9aa4f
tonganh <tongngocanhcampha@gmail.com>
2020-08-15 13:50:02 +0700

    Dog eat method

--- Dog.js
+++ Dog.js
@@ -1,6 +1,12 @@
-function Dog() {
-    this.stomach=[];
+var chalk = require('chalk');
+function Dog(name) {
+  this.name = name;
+  this.stomach = [];
 }
-Dog.prototype.eat=function (cat) {
-    this.stomach.push(cat);
-}
+Dog.prototype.eat = function(cat) {
+  this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function() {
+  console.log('Hi i am a dog ' + chalk.blue(this.name));
+};
+ module.exports = Dog;
