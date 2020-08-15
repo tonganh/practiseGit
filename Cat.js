1c10db8908cfaba9fd002d48590e519988a10d2b
tonganh <tongngocanhcampha@gmail.com>
2020-08-15 13:24:58 +0700

    Reset-checkout

--- Cat.js
+++ Cat.js
@@ -1,3 +1,8 @@
 function Cat(name) {
-    this.name=name;
+  this.name = name;
+  this.stomach = [];
 }
+Cat.prototype.eat = function(mouse) {
+  this.stomach.push(mouse)
+}
+module.exports=Cat;
--- Mouse.js
+++ Mouse.js
@@ -1,3 +1,7 @@
-function Mouse(name){
-    this.name=name;
+function Mouse(name) {
+  this.name = name;
+  this.dead = false;
+}
+Mouse.prototype.die = function() {
+  this.dead = true;
 }
