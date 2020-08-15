328daa33d6e6c673d2fe82989663d321b55b8f90
tonganh <tongngocanhcampha@gmail.com>
2020-08-15 15:08:11 +0700

    Sleep method

--- Mouse.js
+++ Mouse.js
@@ -1,7 +1,11 @@
 function Mouse(name) {
   this.name = name;
   this.dead = false;
-}
+};
 Mouse.prototype.die = function() {
   this.dead = true;
-}
+};
+Mouse.prototype.sleep = function() {
+  console.log('Sleeping...');
+};
+module.exports = Mouse;
